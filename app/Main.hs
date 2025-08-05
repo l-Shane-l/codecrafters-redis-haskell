366ec4c2a75c54f37ce5417206d70c59009f3d70
Shane <shane@peregrinum.dev>
2025-08-05 04:12:27 +0200

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -4,36 +4,76 @@
 
 module Main (main) where
 
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Data.Void
 import Network.Simple.TCP (HostPreference (HostAny), Socket, closeSock, recv, send, serve)
 import System.IO (BufferMode (NoBuffering), hPutStrLn, hSetBuffering, stderr, stdout)
+import Text.Megaparsec
+import Text.Megaparsec.Byte
+import qualified Text.Megaparsec.Byte.Lexer as L
+
+type Parser = Parsec Void ByteString
+
+-- Parse RESP protocol
+data RESP
+  = Array Int [RESP]
+  | BulkString ByteString
+  deriving (Show, Eq)
+
+-- Simple RESP parser
+resp :: Parser RESP
+resp = array <|> bulkString
+ where
+  array = do
+    char 42
+    n <- L.decimal
+    string "\r\n"
+    elements <- count n resp
+    return (Array n elements)
+
+  bulkString = do
+    char 36
+    len <- L.decimal
+    string "\r\n"
+    str <- takeP Nothing len
+    string "\r\n"
+    return (BulkString str)
+
+-- Parse and handle commands
+handleClient :: Socket -> IO ()
+handleClient socket = loop BS.empty
+ where
+  loop buffer = do
+    maybeData <- recv socket 4096
+    case maybeData of
+      Nothing -> return ()
+      Just newData -> processInput (buffer <> newData)
+
+  processInput input =
+    case runParser resp "" input of
+      Left _ -> loop input -- Need more data
+      Right (Array 1 [BulkString "PING"]) -> do
+        send socket "+PONG\r\n"
+        -- Find how much we consumed and process the rest
+        let consumed = BS.length "*1\r\n$4\r\nPING\r\n"
+        processInput (BS.drop consumed input)
+      Right _ -> do
+        send socket "-ERR unknown command\r\n"
+        closeSock socket
 
 main :: IO ()
 main = do
-  -- Disable output buffering
   hSetBuffering stdout NoBuffering
   hSetBuffering stderr NoBuffering
 
-  -- You can use print statements as follows for debugging, they'll be visible when running tests.
   hPutStrLn stderr "Logs from your program will appear here"
 
-  -- Uncomment this block to pass stage 1
   let port = "6379"
   putStrLn $ "Redis server listening on port " ++ port
+
   serve HostAny port $ \(socket, address) -> do
     putStrLn $ "successfully connected client: " ++ show address
-    maybeMsg <- recv socket 4096
-    case maybeMsg of
-      Nothing -> putStrLn "Nothing to see here"
-      Just msg
-        | BS.null msg -> sendAndPrint "Empty message" socket
-        | BS.length msg > 100 -> sendAndPrint "Large message" socket
-        | msg == "*1\r\n$4\r\nPING\r\n" -> sendAndPrint "+PONG\r\n" socket
-        | otherwise -> sendAndPrint "Received normal message" socket
-
+    handleClient socket
     closeSock socket
 
-sendAndPrint :: BS.ByteString -> Socket -> IO ()
-sendAndPrint msg socket = do
-  print msg
-  send socket msg
